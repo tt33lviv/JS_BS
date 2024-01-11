1ba8a4979f1f78a69b91e3a44943aafd75a85b4e
Vitaliy <Shkrobut.medias@gmail.com>
2024-01-11 21:45:07 +0200

    func 003

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function myFn(a, b) {
-    return a + b
-    
+    return a + b   
 }
+
 
 function myLog(c) {
     console.log(c)
@@ -10,3 +10,17 @@ function myLog(c) {
 const c = myFn(10, 10)
 
 myLog(c)
+
+//////
+const personOne = {
+    name: 'Vitaliy',
+    age: 39
+}
+
+function increasePersonAge(person) {
+    person.age += 1
+    return person
+}
+
+increasePersonAge(personOne)
+console.log(personOne.age)
